81ee3780eb0a79220911b2ee80063f1b997f4f8c
le-krogoth <krogoth@mzd.org.uk>
2018-04-22 12:57:16 +0200

    Introducing AP class for wifi scanner

---
+++ src/pod/AP.h
@@ -0,0 +1,36 @@
+/*-----------------------------------------------------------------------------
+ **
+ ** - Proteus -
+ **
+ ** Copyright 2018 by Krogoth and the contributing authors
+ **
+ ** This program is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU Affero General Public License as published by the
+ ** Free Software Foundation, either version 3 of the License, or (at your option)
+ ** any later version.
+ **
+ ** This program is distributed in the hope that it will be useful, but WITHOUT
+ ** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ ** FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ ** for more details.
+ **
+ ** You should have received a copy of the GNU Affero General Public License
+ ** along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **
+ ** -----------------------------------------------------------------------------*/
+#ifndef proteus_ap_h
+#define proteus_ap_h
+
+#include <Arduino.h>
+
+class AP
+{
+public:
+    String ssid;
+    int32_t rssi;
+    int32_t channel;
+    uint8_t encryptionType = 0;
+};
+
+
+#endif //proteus_ap_h
